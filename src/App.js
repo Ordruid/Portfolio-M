028c7f1e0e39d9830bfcddad4377240707692d01
Brian Hobby <hobbytech2001@yahoo.com>
2022-08-17 15:02:21 -0400

    rework

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import Nav from "./Nav";
+import "./Nav.css";
+
+function App() {
+  return <Nav />;
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+export default function Nav() {
+  return (
+    <nav className="nav">
+      <a href="/" className="site-title">
+        Brian Hobby Junior Dev
+      </a>
+      <ul>
+        <li>
+          <a href="/About">About Me</a>
+        </li>
+        <li>
+          <a href="/Resume">Resume</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
