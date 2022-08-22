168eb51c1299a825b9be98d666c561edb34fbb14
Brian Hobby <hobbytech2001@yahoo.com>
2022-08-22 16:33:10 -0400

    content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
 export default function Home() {
   return (
-  <div className="page">
-  <h1>Home</h1>
-  </div>
+    <div className="page">
+      <h1>Home</h1>
+      <div className="text">
+        <h4>
+          My name is Brian Hobby and I'm an aspiring Javascript junior engineer
+          looking to break into the field for the first time! I started my
+          journey in October of 2021 and have worked in many different
+          programming environments since. Programming for me initially started
+          out as a career change that would utilize my talents in the
+          technosphere, giving me a better quality of work and life. Halfway
+          into my journey I quickly discovered a different perspective, it
+          became a passion. I found myself developing a sense of pride every
+          time I overcame a new obstacle and added more knowledge and ability in
+          my chosen field. Then I began to share that passion and excitement
+          with my friends, family, and soon fellow aspiring developers which
+          opened an entirely new world of possibilities and perspective. I hope
+          to eventually find a career with others that share this passion and
+          excitement in order to help build something truly wonderful.
+        </h4>
+      </div>
+    </div>
   );
 }
