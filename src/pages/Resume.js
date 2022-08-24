03184567f630b5732488b162271e69d93c3db9fc
Brian Hobby <hobbytech2001@yahoo.com>
2022-08-24 13:52:14 -0400

    img testing

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,7 @@
 export default function Resume() {
   return (
     <div>
-      <img src="./img/Resume.jpg" alt="Resume" height="1000" width="800"></img>
+      <img src="/Resume.jpg" alt="Resume" height="1000" width="800"></img>
     </div>
   );
 }
